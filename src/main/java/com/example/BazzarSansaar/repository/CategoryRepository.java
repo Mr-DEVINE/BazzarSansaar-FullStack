fe333ad268e74e287023308bbe731ca1586fb589
Sachin Purbey <sachinpurbey2058@gmail.com>
2025-08-30 02:50:13 +0530

    feat: Create Product and Category repositories

---
+++ src/main/java/com/example/BazzarSansaar/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.BazzarSansaar.repository;
+
+import com.example.BazzarSansaar.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
