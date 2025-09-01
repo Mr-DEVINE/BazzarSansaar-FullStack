9cd7f40d05073bf83df60881624579d5fa7d44b8
Sachin Purbey <sachinpurbey2058@gmail.com>
2025-09-02 04:02:45 +0530

    feat: Create Product and Category entities and  their repositories

---
+++ backend/src/main/java/com/example/BazzarSansaar/model/Category.java
@@ -0,0 +1,26 @@
+package com.example.BazzarSansaar.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<Product> products;
+}
+
---
+++ backend/src/main/java/com/example/BazzarSansaar/model/Product.java
@@ -0,0 +1,32 @@
+package com.example.BazzarSansaar.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @JsonBackReference
+    private Category category;
+}
+
