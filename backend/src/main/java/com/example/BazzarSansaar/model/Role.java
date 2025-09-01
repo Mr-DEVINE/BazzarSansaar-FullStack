ce73214b82ad45210b0cd97a69b692149cba12e8
Sachin Purbey <sachinpurbey2058@gmail.com>
2025-09-02 03:47:08 +0530

    feat: Create User and Role JPA entities

---
+++ backend/src/main/java/com/example/BazzarSansaar/model/Role.java
@@ -0,0 +1,26 @@
+package com.example.BazzarSansaar.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 60)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
+
