6e0a35f02f09918b9971bd7565b350e0686f486c
Sachin Purbey <sachinpurbey2058@gmail.com>
2025-09-02 03:42:42 +0530

    feat: Add core Spring Boot application files and configuration

---
+++ backend/src/main/java/com/example/BazzarSansaar/BazzarSansaarApplication.java
@@ -0,0 +1,13 @@
+package com.example.BazzarSansaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazzarSansaarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazzarSansaarApplication.class, args);
+    }
+
+}
